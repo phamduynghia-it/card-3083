61237815c4955ad7cac94d21abad5db08360cc03
phamduynghia-it <162310524+phamduynghia-it@users.noreply.github.com>
2026-07-14 07:15:59 +0700

    Deploy greeting card with dynamic assets and custom workflow

---
+++ config.js
@@ -0,0 +1,12 @@
+const defaultData = {
+    template: "love",
+    data: {
+        title: `Happy birthday 💖`,
+        type_music: "link",
+        music_id: `./assets/user_music.mp3`,
+        backgroundMusic: `./assets/user_music.mp3`,
+        popupMessage: `🎉 Chúc mừng sinh nhật Tình Đặng! Tuổi mới tiền vào như nước, hạnh phúc tràn trề , drama tự né còn nhan sắc thì mãi dừng ở tuổi 18 🥳`,
+        messages: ["🎉 Chúc mừng sinh nhật Tình Đặng!"," Tuổi mới tiền vào như nước"," hạnh phúc tràn trề","drama tự né","còn nhan sắc thì mãi dừng ở tuổi 18 🥳"],
+        images: ["./assets/user_images_0.jpg","./assets/user_images_1.jpg","./assets/user_images_2.jpg","./assets/user_images_3.jpg","./assets/user_images_4.jpg","./assets/user_images_5.jpg","./assets/user_images_6.jpg"]
+    }
+};
